9d76cf7bb9a25c53b5572479f92c90a64b2f2aea
Filippp554 <91588605+Filippp554@users.noreply.github.com>
2022-01-12 08:28:45 +0100

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	cout << "Zadaj svoj vek:" << endl;
+
+	int vek;
+	cin >> vek;
+
+	if (vek >= 18)
+	{
+		cout << "si plnolety" << endl;
+
+	}
+	else {
+
+		cout << "nie si plnolety" << endl;
+	}
+
+
+
+
+
+}
